c2ee5a3c2b8e44c6be160c4d4d2df1d7f537319c
Chris Helmich <kagekirin@gmail.com>
2021-10-22 12:54:40 +0900

    Implement 'guid' generation and meta file output
    Implementation note: Standart.Hash.xxHash does not have
    xxh3 128bit hashes, so we hand-roll our own 'guid'
    by generating 2 64bit hashes, with the 'lower' hash
    being the filename seeded with the 'upper' hash.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,92 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+using Standart.Hash.xxHash;
 
 namespace metagen
 {
+    internal struct Guid
+    {
+        public UInt64 upper;
+        public UInt64 lower;
+
+        public override string ToString()
+        {
+            return $"{upper:x8}{lower:x8}";
+        }
+    }
+    internal class ProgramOptions
+    {
+        [Option('s', "seed",
+            Default = "ffd",
+            HelpText = "seed string, e.g. package name",
+            Required = false)]
+        public string Seed { get; set; }
+
+        [Value(0,
+            Min = 1,
+            MetaName = "input files",
+            HelpText = "files to generate .meta files for",
+            Required = true)]
+        public IEnumerable<string> Files { get; set; }
+    };
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandLine.Parser.Default.ParseArguments<ProgramOptions>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+        }
+
+        static void RunOptions(ProgramOptions opts)
+        {
+            UInt64 seed = xxHash64.ComputeHash(Encoding.UTF8.GetBytes(opts.Seed));
+            Console.WriteLine($"seed: {opts.Seed} -> {seed:x8}");
+
+            foreach (var f in opts.Files)
+            {
+                if (File.Exists(f) && f.EndsWith(@".meta"))
+                {
+                    continue;
+                }
+                else if (File.Exists(f) || Directory.Exists(f))
+                {
+                    Guid guid = new Guid();
+                    guid.upper = xxHash64.ComputeHash(Encoding.UTF8.GetBytes(f), seed);
+                    guid.lower = xxHash64.ComputeHash(Encoding.UTF8.GetBytes(f), guid.upper);
+
+                    Console.WriteLine($"{f} -> {guid}");
+
+                    generateMetaFile(f, guid);
+                }
+            }
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
+        }
+
+        static void generateMetaFile(string filename, Guid guid)
+        {
+            var metaPath = filename + @".meta";
+            var metaContents = "fileFormatVersion: 2\n"
+            + $"guid: {guid}\n"
+            + @"MonoImporter:
+  externalObjects: {}
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant: 
+";
+            File.WriteAllText(metaPath, metaContents);
         }
     }
 }
